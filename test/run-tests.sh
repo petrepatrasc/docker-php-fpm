685db166fc66fe64e2b842d9089b9939e22ec594
Petre Pătraşc <petre@dreamlabs.ro>
2015-10-13 02:50:31 +0300

    Add assertions for failing tests

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -2,22 +2,26 @@
 
 PHP_VERSION_INSTALLED=$(docker exec php_dev php --version | grep "PHP 5.6")
 if [ -z "$PHP_VERSION_INSTALLED" ]; then
+  echo "PHP 5.6 is not installed."
   exit 1
 fi
 
 $(docker exec php_dev nc -z localhost 9000)
 PHP_PORT_IS_OPEN=$?
 if [ "$PHP_PORT_IS_OPEN" -eq 1 ]; then
+  echo "Port is not exposed."
   exit 1
 fi
 
 MAX_EXECUTION_TIME_IS_OVERWRITTEN=$(docker exec php_dev cat /etc/php5/fpm/conf.d/99-system.ini | grep "max_execution_time = 60")
 if [ -z "$MAX_EXECUTION_TIME_IS_OVERWRITTEN" ]; then
+  echo "Max execution time has not been overwritten."
   exit 1
 fi
 
 XDEBUG_TRACE_IS_OVERWRITTEN=$(docker exec php_dev cat /etc/php5/fpm/conf.d/99-system.ini | grep "xdebug.trace_enable_trigger = 0")
 if [ -z "$XDEBUG_TRACE_IS_OVERWRITTEN" ]; then
+  echo "Xdebug trace has not been overwritten."
   exit 1
 fi
 
