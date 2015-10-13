fe2b6b1e01c3eac0eb7f7793293019569729096d
Petre Pătraşc <petre@dreamlabs.ro>
2015-10-13 03:04:54 +0300

    Add sudo to docker test commands

--- test/start-test-container.sh
+++ test/start-test-container.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker build -t petrepatrasc/docker-php-fpm-development . && \
-docker run -d \
+sudo docker build -t petrepatrasc/docker-php-fpm-development . && \
+sudo docker run -d \
   --name php_dev \
   -e PHP_CONF_MAX_EXECUTION_TIME=60 \
   -e PHP_CONF_XDEBUG_TRACE_ENABLE_TRIGGER=0 \
